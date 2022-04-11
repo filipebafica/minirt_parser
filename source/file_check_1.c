a7768d38df004f11af10df68b32341600e0f23d5
fbafica <fbafica@student.42sp.org.br>
2022-04-10 22:33:43 -0300

    wip

--- source/file_check_1.c
+++ source/file_check_1.c
@@ -6,13 +6,33 @@
 /*   By: fbafica <fbafica@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 16:55:34 by fbafica           #+#    #+#             */
-/*   Updated: 2022/04/10 21:27:48 by fbafica          ###   ########.fr       */
+/*   Updated: 2022/04/10 22:33:23 by fbafica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers.h"
 
-int	file_check(char *file_name, t_parameters *parameters)
+static int	check_for_obj(char **tokens, t_parameters *p)
+{
+	return (0);
+}
+
+static int	check_for_l(char **tokens, t_parameters *p)
+{
+	return (0);
+}
+
+static int	check_for_c(char **tokens, t_parameters *p)
+{
+	return (0);
+}
+
+static int	check_for_a(char **tokens, t_parameters *p)
+{
+	// call function to count parameters frequecy
+}
+
+int	file_check(char *file_name)
 {
 	char	**tokens;
 
@@ -20,14 +40,16 @@ int	file_check(char *file_name, t_parameters *parameters)
 		return (send_error("Error\nBad extension"));
 	if (load_file(file_name, &tokens) == -1)
 		return (send_error("Error\nCould not open the file"));
-	if (check_for_a(tokens, parameters) == -1)
-		send_error("Error\nThere's no any Ambient Lightning parameter"))
-	else if (check_for_c(tokens, parameters) == -1)
-		send_error("Error\nThere's no any Camera parameter"))
-	else if (check_for_l(tokens, parameters) == -1)
-		send_error("Error\nThere's no any Light parameter"))
-	else if (check_for_objects(tokens, parameters) == -1)
-		send_error("Error\nThere's no any Object parameter"))
+	if (check_for_tab(tokens, p) == -1)
+		return (send_error("Error\n'\\t' is not a valid character"));
+	if (check_for_a(tokens, p) == -1)
+		return (send_error("Error\nThere's no Ambient Lightning parameter"));
+	else if (check_for_c(tokens, p) == -1)
+		return (send_error("Error\nThere's no Camera parameter"));
+	else if (check_for_l(tokens, p) == -1)
+		return (send_error("Error\nThere's no Light parameter"));
+	else if (check_for_obj(tokens, p) == -1)
+		return (send_error("Error\nThere's no Object parameter"));
 	free_tokens(tokens);
 	return (0);
 }
--- source/utils_2.c
+++ source/utils_2.c
@@ -1,33 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   headers.h                                          :+:      :+:    :+:   */
+/*   utils_2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbafica <fbafica@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/10 14:01:39 by fbafica           #+#    #+#             */
-/*   Updated: 2022/04/10 22:32:31 by fbafica          ###   ########.fr       */
+/*   Created: 2022/04/10 22:19:51 by fbafica           #+#    #+#             */
+/*   Updated: 2022/04/10 22:23:15 by fbafica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HEADERS_H
-# define HEADERS_H
-# include "libft.h"
-# include <fcntl.h>
-# include <stdlib.h>
-
-typedef struct s_parameters
-{
-	
-}	t_parameters;
-
-				/*
-				* FILE PARSER 
-				*/
-int			file_check(char *file_name, t_parameters *p);
-int			load_file(char *file_name, char ***tokens);
-int			send_error(char *error_message);
-void		free_tokens(char **tokens);
-void		print_tokens(char **tokens);
-
-#endif
+#include "headers.h"
